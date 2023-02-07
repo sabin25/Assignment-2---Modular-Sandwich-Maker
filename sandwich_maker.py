df7380fcdd2d18872b386547fb73e8755474bac9
Sabin Shrestha <sabinsabie@gmail.com>
2023-02-06 22:48:43 -0500

    sandwich_maker

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -5,14 +5,12 @@ class SandwichMaker:
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if it cannot be made due to lack of resources."""
         for ingredient, amount in ingredients.items():
-           if self.machine_resources[ingredient] >= amount:
-               return True
-           else:
-               return False
+            if self.machine_resources[ingredient] >= amount:
+                return True
+            else:
+                return False
 
     def make_sandwich(self, size, ingredients):
-         """Decreases the ingredients from machine resources after making sandwich."""
-         for ingredient, amount in ingredients.items():
+        """Decreases the ingredients from machine resources after making sandwich."""
+        for ingredient, amount in ingredients.items():
             self.machine_resources[ingredient] -= amount
-
-
